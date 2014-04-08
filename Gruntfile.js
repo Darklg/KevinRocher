8ffb9eea6cdbace81c72635be59e96599195774d
Darklg <kev.rocher@gmail.com>
2014-04-08 17:39:28 +0200

    Complete build & dev mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,9 @@
 module.exports = function(grunt) {
+    var prodCSS = '_prod/assets/css/',
+        prodJS = '_prod/assets/js/',
+        uglifyFiles = {};
+
+    uglifyFiles[prodJS + 'main.js'] = prodJS + 'main-tmp.js';
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jekyll: {
@@ -9,9 +14,39 @@ module.exports = function(grunt) {
             },
             dev: {}
         },
+        concat: {
+            css: {
+                src: [
+                    prodCSS + 'dev--cssc-default.css',
+                    prodCSS + 'dev--cssc-common.css',
+                ],
+                dest: prodCSS + 'dev--main.css'
+            },
+            js: {
+                src: [
+                    prodJS + 'app/functions.js',
+                    prodJS + 'app/events.js',
+                ],
+                dest: prodJS + 'main-tmp.js'
+            },
+        },
+        cssmin: {
+            css: {
+                src: prodCSS + 'dev--main.css',
+                dest: prodCSS + 'main.css'
+            }
+        },
+        uglify: {
+            js: {
+                files: uglifyFiles,
+            },
+        },
         shell: {
             jekyll: {
                 command: 'jekyll build;'
+            },
+            postconcat: {
+                command: 'rm ' + prodCSS + 'dev--*.css; rm -r ' + prodJS + 'app/; rm -r ' + prodJS + 'main-tmp.js; '
             }
         },
         watch: {
@@ -25,11 +60,24 @@ module.exports = function(grunt) {
     // Load modules
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-jekyll');
     grunt.loadNpmTasks('grunt-shell');
 
     // Tasks
-    grunt.registerTask('default', []);
-    grunt.registerTask('dev', ['jekyll:dev']);
-    grunt.registerTask('build', ['shell:jekyll']);
+    grunt.registerTask('default', [
+        'jekyll:dev'
+    ]);
+    grunt.registerTask('build', [
+        // Launch Jekyll
+        'shell:jekyll',
+        // Concat CSS/JS & uglify
+        'concat',
+        'cssmin',
+        'uglify',
+        // Clean up
+        'shell:postconcat'
+    ]);
 };
