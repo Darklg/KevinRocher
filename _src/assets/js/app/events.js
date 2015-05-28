745175add61b3bc780f42e893340d04225c48f35
Darklg <kev.rocher@gmail.com>
2015-05-28 14:49:05 +0200

    lower offset

--- _src/assets/js/app/events.js
+++ _src/assets/js/app/events.js
@@ -4,7 +4,9 @@ window.domReady(function() {
     set_mainmenu();
     set_scroll();
     var scrollItems = document.querySelectorAll('.section-header, .projects-list li, .skills-list li, .clients-list li, .cc-contact p, .hellomynameiskevin img, .about-content p, .social-networks li'),
-        scrollAnim = new dkJSUScrollAnims(scrollItems);
+        scrollAnim = new dkJSUScrollAnims(scrollItems, {
+            offsetY: -50
+        });
 
     for (var i = 0, len = scrollItems.length; i < len; i++) {
         scrollItems[i].className += " has-scrollanim";
