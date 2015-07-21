8ee009e2b05a521569c4898322736669909def46
Darklg <kev.rocher@gmail.com>
2015-07-21 14:00:06 +0200

    Content, code & anim enhancement
    * Label availability in config. + date update.
    * Add a footer quote with tiptap.
    * Higher contact area.
    * Better split of the big fat main CSS File.

--- _src/assets/js/app/events.js
+++ _src/assets/js/app/events.js
@@ -1,9 +1,9 @@
 window.domReady(function() {
-
+    document.body.setAttribute('data-nojs', '0');
     set_separators();
     set_mainmenu();
     set_scroll();
-    var scrollItems = document.querySelectorAll('.section-header, .projects-list li, .skills-list li, .clients-list li, .cc-contact p, .hellomynameiskevin img, .about-content p, .social-networks li'),
+    var scrollItems = document.querySelectorAll('.section-header, .projects-list li, .skills-list li, .clients-list li, .cc-contact p, .hellomynameiskevin img, .about-content p, .social-networks li, .contact-avatar img, .footer q, .footer cite'),
         scrollAnim = new dkJSUScrollAnims(scrollItems, {
             offsetY: -50
         });
@@ -12,6 +12,14 @@ window.domReady(function() {
         if (scrollItems[i].className != 'has-scrollanim') {
             scrollItems[i].className += " has-scrollanim";
         }
+    }
+
+    var q = document.getElementById('footer-quote');
+    if (q && document.addEventListener) {
+        var tiptap = new dkJSUTipTapTip(q);
+        q.addEventListener('activescrollanim', function launch_tiptap_q() {
+            tiptap.launch();
+        }, 1);
     }
 
 });
