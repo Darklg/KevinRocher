5514995e93e92d2d6857cca6d955317f5e3c903c
Darklg <kev.rocher@gmail.com>
2016-01-01 13:16:24 +0100

    Better Grunt watch

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,6 +1,18 @@
 module.exports = {
+    css: {
+        files: [
+            '_src/assets/scss/*.scss',
+            '_src/assets/scss/*/*.scss'
+        ],
+        tasks: ['compass']
+    },
     jekyll: {
-        files: ['_src/*.html', '_src/_includes/*.html'],
+        files: [
+            '_src/*.html',
+            '_src/_includes/*.html',
+            '_src/assets/css/main.css',
+            '_src/assets/js/*.js'
+        ],
         tasks: ['jekyll:dev']
     }
 };
